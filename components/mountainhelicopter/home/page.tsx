9fb5ec6123f6b38ed94749ad3dfa4ba8954233d7
Aakrit_Adhikari <aakritadhikari2002@gmail.com>
2026-07-24 21:12:32 +0545

    conflict resolved

--- components/mountainhelicopter/home/page.tsx
+++ components/mountainhelicopter/home/page.tsx
@@ -5,28 +5,18 @@ import Rescue from "./Rescue";
 import Blogs from "./Blogs";
 import Cta from "./CTA";
 import Faq from "./Faq";
-<<<<<<< HEAD
-import Stats from "./Stats";
-import InsagramSection from "./InstagramSection";
-=======
-import StateSection from "./HomeStats"
->>>>>>> fe_aakrit
 
 const page = () => {
   return (
     <div>
       <Hero />
-<<<<<<< HEAD
-      <Stats />
-=======
-      <StateSection/>
->>>>>>> fe_aakrit
+   
       <Story />
       <Rescue />
-      <InsagramSection />
       <Blogs />
       <Faq />
       <Cta />
+    
     </div>
   );
 };
