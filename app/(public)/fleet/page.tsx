56b7aaf640a9a2b5dabe50b800a41914a73852ce
Bashistha1307 <nepal.bashistha@gmail.com>
2026-07-26 12:15:25 +0545

    fleet page

---
+++ app/(public)/fleet/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/mountainhelicopter/fleet/Hero";
+import Showcase from "@/components/mountainhelicopter/fleet/Showcase";
+import Aircraft from "@/components/mountainhelicopter/fleet/Aircraft";
+import Cta from "@/components/mountainhelicopter/home/CTA";
+
+export default function FleetPage() {
+  return (
+    <>
+      <Hero />
+      <Showcase />
+      <Aircraft />
+      <Cta />
+    </>
+  );
+}
