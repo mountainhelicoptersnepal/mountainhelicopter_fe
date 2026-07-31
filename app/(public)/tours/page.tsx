efad1cbd15d477f9952fc789885c0ab500cfe4eb
Bashistha1307 <nepal.bashistha@gmail.com>
2026-07-31 16:42:28 +0545

    principle tour

--- app/(public)/tours/page.tsx
+++ app/(public)/tours/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/mountainhelicopter/tours/Hero";
+import Principle from "@/components/mountainhelicopter/tours/Principle";
 
 export default function ToursPage() {
   return (
     <>
       <Hero />
+      <Principle />
     </>
   );
 }
