b5e34037c49d8a7368c3c79dce09e9627f6bd23b
SamyakMdr <samyak11manandhar@gmail.com>
2026-07-31 11:28:26 +0545

    Delete Inquire page.

--- app/(public)/inquire/page.tsx
+++
@@ -1,9 +0,0 @@
-import InquiryForm from "@/components/mountainhelicopter/inquire/InquiryForm";
-
-export default function InquiryPage() {
-  return (
-    <>
-      <InquiryForm />;
-    </>
-  );
-}
